62f0585de537e3133bb9761e1f970d596abb5561
robstowe <robstowe94@gmail.com>
2023-05-23 18:25:51 -0700

    added in local storage functionality

--- assets/script.js
+++ assets/script.js
@@ -10,12 +10,6 @@ var questions = [{
   title: "What does Javascript do?",
   choices: ["It's a coffee maker, duh.", "Sir, this is a Wendy's.", "Adds functionality to websites", "example add this in later"],
   correctAnswer: "Adds functionality to websites"
-
-},
-{
-  title: "",
-  choices: "",
-  correctAnswer: "",
 }];
 let questionIndex = 0;
 
@@ -28,9 +22,9 @@ let startBtn = document.querySelector('#start-button');
 let titleEl = document.querySelector('#title');
 let choicesEl = document.querySelector('#choices');
 let timerEl = document.querySelector('#timer');
-let timer 
-let timerCount
-let quizScore = 0
+let timer;
+let timerCount;
+let quizScore = 0;
 
 //adding the class hidden from the CSS file to this element
 startScreenEl.setAttribute('class', 'reveal');
@@ -39,7 +33,7 @@ startEndEL.setAttribute('class', 'hidden');
 timerEl.setAttribute('class', 'countdown');
 
 function startQuiz() {
-  timerCount = 30;
+  timerCount = 15;
   startScreenEl.setAttribute('class', 'hidden');
   startQuestionEl.setAttribute('class', 'reveal');
   generateQuestions();
@@ -50,7 +44,7 @@ function beginTimer() {
   timer = setInterval(function(){
     timerCount--
     timerEl.textContent = timerCount
-    if(timerCount === 0){
+    if(timerCount <= 0){
       clearInterval(timer)
       lostGame();
     }
@@ -81,9 +75,83 @@ function generateQuestions() {
   
     tempBtn.onclick = validateAnswer//run the validateAnswer function
     choicesEl.appendChild(tempBtn) //we are appending this to the Dom
- 
+  }
+  if (currentQuestion === 0){
+    endQuiz();
   }
 }
+
+
+function validateAnswer(){
+  if (this.id !== questions[questionIndex].correctAnswer){
+    // alert("wrong")
+    console.log('this is the id', this.id)
+    subtractQuizTimer()
+  }else {
+    // alert("yay")
+    quizScore += 10
+    console.log('score so far', quizScore)
+  }
+  questionIndex++;
+  if (questionIndex >= questions.length){
+    endQuiz();
+  } else {
+    generateQuestions();
+  }
+}
+
+function endQuiz() {
+  startQuestionEl.setAttribute('class', 'hidden');
+  startEndEL.setAttribute('class', 'reveal');
+  localStorage.setItem("recentScore", quizScore);
+  clearInterval(timer)
+};
+
+function subtractQuizTimer() {
+  timerCount -= 3;
+}
+
+var startButton = document.querySelector("#startButton");
+var endButton = document.querySelector('#end-screen');
+var initials = document.querySelector('#initials');
+var initialsButton = document.querySelector('#initials-button');
+var hiscores = document.querySelector('#hiscores');
+// creating the array for the user data, and parsing thru JSON, and getting it via localstorage
+var userData = JSON.parse(localStorage.getItem("hiscores")) || [];
+
+initialsButton.addEventListener("click", function(){
+  // localStorage.setItem("Initials", initials);
+  // localStorage.setItem("recentScore", quizScore);
+  
+  //this is object syntax, key value pairs
+  var Player = {
+    initials:initials.value, //the key goes to the left of the colon, and what you want to pair up the key with, goes to the right
+    score:quizScore,
+    
+  }
+  console.log(Player);
+  userData.push(Player) //Push places the user data, within the userData array. Adds the item to the array
+  // here we are stringifying the userdata array so local storage will accept it. localstorage only accepts strings 
+  localStorage.setItem("hiscores", JSON.stringify(userData));
+  for (let i = 0; i < userData.length; i++) {
+    var p = document.createElement('p') //create the variable p, and then create the P tag, to the hiscores section
+    p.textContent = "Player: " + userData[i].initials + " Score: " + userData[i].score; //this adds the text content to the page, and organizes it by the information within the array, using initials and score
+    hiscores.append(p) //Append places the data on the page
+    
+  }
+})
+
+startBtn.onclick = startQuiz;
+
+
+//I need to put a page together "this is the End"
+//add a button where they can take the quiz again
+//add a hiscores page
+
+//to display a score, if (questionindex = 4)
+//alert (show recent score)
+//end game
+//to get the score it will be localStorage.getItem("recentScore", quizScore)
 
 //this function is generating the questions & attributing the timer to the game
 // function validateAnswer(e) {
@@ -113,44 +181,3 @@ function generateQuestions() {
 //   questionIndex++;
 //   generateQuestions();
 // };
-
-function validateAnswer(){
-  if (this.id !== questions[questionIndex].correctAnswer){
-    // alert("wrong")
-    console.log('this is the id', this.id)
-    subtractQuizTimer()
-    questionIndex++;
-    generateQuestions();
-  }else {
-    // alert("yay")
-      questionIndex++;
-      generateQuestions();
-      quizScore += 10
-      console.log('score so far', quizScore)
-  }
-}
-
-function endQuiz() {
-  generateQuestions();
-  startEndEL.setAttribute('class', 'reveal');
-  localStorage.setItem("recentScore", quizScore)
-};
-
-function subtractQuizTimer() {
-  timerCount -= 15;
-}
-
-var startButton = document.querySelector("#startButton");
-var endButton = document.querySelector('#end-screen');
-
-startBtn.onclick = startQuiz;
-
-
-//I need to put a page together "this is the End"
-//add a button where they can take the quiz again
-//add a hiscores page
-
-//to display a score, if (questionindex = 4)
-//alert (show recent score)
-//end game
-//to get the score it will be localStorage.getItem("recentScore", quizScore)
